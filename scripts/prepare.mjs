23800e8111cea64803ef0b635191d5d1f5056870
Mohammad Toosi <m_dd@rogers.com>
2023-08-21 02:38:59 +0330

    feat: add husky and lint-staged

---
+++ scripts/prepare.mjs
@@ -0,0 +1,16 @@
+import os from 'node:os';
+import { exec } from 'node:child_process';
+import ci from 'ci-info';
+
+if (!ci.isCI) {
+  exec('husky install', (_, stdout) => {
+    console.log(stdout);
+  });
+}
+
+if (!os.type().toLowerCase().includes('windows') && !ci.isCI) {
+  console.log('Enabling husky on non Windows systems');
+  exec('chmod ug+x .husky/*', (_, stdout) => {
+    console.log(stdout);
+  });
+}
