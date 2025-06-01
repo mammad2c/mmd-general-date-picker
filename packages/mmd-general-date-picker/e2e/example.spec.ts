da3f065f5c18e95072abb2ac0c8f68f2ed11836b
Mohammad Toosi <m_dd@rogers.com>
2025-06-01 16:48:26 +0200

    feat: add support for e2e component testing

--- packages/mmd-general-date-picker/e2e/example.spec.ts
+++ packages/mmd-general-date-picker/e2e/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from "@playwright/test";
 
+// Extend the Window interface to include MmdGeneralDatePicker
+declare global {
+  interface Window {
+    MmdGeneralDatePicker: typeof import("../src/index").default;
+  }
+}
+
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("http://localhost:3000");
+
+  page.evaluate(() => {
+    new window.MmdGeneralDatePicker({
+      element: "#app",
+    });
+  });
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+  await expect(page).toHaveTitle(/E2E Test Environment/);
 });
